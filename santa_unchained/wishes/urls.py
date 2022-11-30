20c281a0361017f66a006528c9a750acb627870e
Vladyslav Fedoriuk <vladyslav.fedoriuk@deployed.pl>
2022-11-30 16:03:20 +0100

    Add a thanks page

--- santa_unchained/wishes/urls.py
+++ santa_unchained/wishes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from santa_unchained.wishes.views import WishListFormView
+from santa_unchained.wishes.views import WishListFormView, WishListSuccessView
 
 app_name = "wishes"
 
-urlpatterns = [path("", WishListFormView.as_view(), name="wishlist")]
+urlpatterns = [
+    path("", WishListFormView.as_view(), name="wishlist"),
+    path("success/<slug:slug>/", WishListSuccessView.as_view(), name="success")
+]
 
--- santa_unchained/wishes/views.py
+++ santa_unchained/wishes/views.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views.generic import TemplateView
 
 from santa_unchained.wishes.forms import WishListWithAddressAndItemsForm
 from django.views.generic.edit import FormView
@@ -14,9 +16,19 @@ class WishListFormView(FormView):
     form_class = WishListWithAddressAndItemsForm
 
     def get_success_url(self):
-        # TODO: replace with success page
-        return reverse("accounts:index")
+        return reverse("wishes:success", kwargs={"slug": self.object.slug})
 
     def form_valid(self, form):
-        form.save()
+        self.object = form.save()
         return super().form_valid(form)
+
+
+class WishListSuccessView(TemplateView):
+
+    template_name = "wish_list_success.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        instance = get_object_or_404(WishList, slug=self.kwargs["slug"])
+        context['wishlist_url'] = instance.get_absolute_url()
+        return context
