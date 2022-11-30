f0a289d8b9d7781d60ebc9888143b16284ddc89d
Vladyslav Fedoriuk <vladyslav.fedoriuk@deployed.pl>
2022-11-30 14:04:30 +0100

    Add gettexts to constants

--- santa_unchained/wishes/models.py
+++ santa_unchained/wishes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
 from django_extensions.db.fields import AutoSlugField
 
 from santa_unchained.wishes.constants import WishListStatuses
