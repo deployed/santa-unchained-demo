c480650c78b7bef7aa0a6c40772a8e2366e03a83
Łukasz Sitko <lukasz.sitko@deployed.pl>
2022-12-02 10:58:02 +0100

    small fixes

--- santa_unchained/accounts/forms.py
+++ santa_unchained/accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 
-from santa_unchained.constants import Role
+from santa_unchained.accounts.constants import Role
 
 
 class GroupAdminForm(forms.ModelForm):
